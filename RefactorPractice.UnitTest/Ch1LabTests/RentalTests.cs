d0c6dc20c9398cd479c529e8a5dbc4e550b2a494
BryanYu <ak78912@hotmail.com>
2017-11-16 20:50:55 +0800

    Before Refactor add stable tests

---
+++ RefactorPractice.UnitTest/Ch1LabTests/RentalTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RefatorPractice.Ch1Lab;
+
+namespace RefactorPractice.UnitTest.Ch1LabTests
+{
+    [TestFixture]
+    public class RentalTests
+    {
+        [Test]
+        public void When_Customer_Add_Rental_Get_Statement()
+        {
+            var customer = new Customer("Bryan");
+            var rental = new Rental(new Movie("Movie1", 0), 2);
+            customer.AddRental(rental);
+            var actual = customer.GetStatement();
+            var expected = @"Rental Record forBryan Movie1 2 Amount owed is 2 You earned 1 frequent renter points";
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+    }
+}
---
+++ RefactorPractice.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RefactorPractice.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RefactorPractice.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65544821-9a45-4209-900d-ce53136b51d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
