cf8be7c5de38d93a367eb2023f70b2507ee7c0ae
BryanYu <ak78912@hotmail.com>
2017-11-16 21:24:24 +0800

    add GetHtmlStatement feature

--- RefactorPractice/Ch1Lab/Customer.cs
+++ RefactorPractice/Ch1Lab/Customer.cs
@@ -41,6 +41,20 @@ namespace RefatorPractice.Ch1Lab
             return result;
         }
 
+        public string GetHtmlStatement()
+        {
+            var result = $@"<h1>Rentals for <EM>{this.Name}</EM></h1><p>\n";
+            this._rental.ForEach(
+                item =>
+                {
+                    result += $@"{item.Movie.Title}: {item.GetCharge()}";
+                });
+            result += $@"<p>You owe<EM>{this.GetTotalCharege()}</EM></p>\n";
+            result +=
+                $@"On this rental you earned <EM>{this.GetTotalFrequentRenterPoints()}</EM> frequent renter points";
+            return result;
+        }
+
         private double GetTotalCharege()
         {
             double result = this._rental.Sum(item => item.GetCharge());
