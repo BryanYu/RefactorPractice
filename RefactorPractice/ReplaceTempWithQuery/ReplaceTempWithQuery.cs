73dc3da7b57c35f27b9b44a6eaed0d69fb3b1b15
BryanYu <ak78912@hotmail.com>
2017-11-22 21:33:04 +0800

    ReplaceTempWithQuery - Before

---
+++ RefactorPractice/ReplaceTempWithQuery/ReplaceTempWithQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.ReplaceTempWithQuery
+{
+    public class ReplaceTempWithQuery
+    {
+        private double _quantity;
+
+        private double _itemPrice;
+
+        public double Sample()
+        {
+            double basePrice = this._quantity * this._itemPrice;
+            if (basePrice > 1000)
+            {
+                return basePrice * 0.95;
+            }
+            else
+            {
+                return basePrice * 0.98;
+            }
+        }
+    }
+}
