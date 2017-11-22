3712422b46be9e40b8b04e6d01959ee2f8dbb236
BryanYu <ak78912@hotmail.com>
2017-11-22 21:26:10 +0800

    InlineTemp - Before

---
+++ RefactorPractice/InlineTemp/InlineTemp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.InlineTemp
+{
+    public class InlineTemp
+    {
+        public bool Sample()
+        {
+            double basePrice = this.GetBasePrice();
+            return (basePrice > 1000);
+        }
+
+        public double GetBasePrice()
+        {
+            return double.MaxValue;
+        }
+    }
+}
