7f7eefb59ca74105e53c3aa57688d1d1f812536e
BryanYu <ak78912@hotmail.com>
2017-11-22 21:48:43 +0800

    IntroduceExplainingVarible - Before

---
+++ RefactorPractice/IntroduceExplainingVarible/IntroduceExplainingVarible.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.IntroduceExplainingVarible
+{
+    public class IntroduceExplainingVarible
+    {
+        private double _quantity = 0;
+
+        private double _itemPrice = 0;
+
+        public double GetPrice()
+        {
+            return this._quantity * this._itemPrice - Math.Max(0, this._quantity - 500) * this._itemPrice * 0.05
+                   + Math.Min(this._quantity * this._itemPrice * 0.1, 100);
+        }
+    }
+}
