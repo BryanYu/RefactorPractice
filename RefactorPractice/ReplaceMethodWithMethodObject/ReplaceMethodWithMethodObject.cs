868693dd5c9fe1ae65c8f81dc021760edaebf9f6
BryanYu <ak78912@hotmail.com>
2017-11-23 21:33:30 +0800

    ReplaceMethodWithMethodObject - Before

---
+++ RefactorPractice/ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.ReplaceMethodWithMethodObject
+{
+    public class ReplaceMethodWithMethodObject
+    {
+        public class Account
+        {
+            public int Gamma(int inputVal, int quantity, int yearToDate)
+            {
+                int importantValue1 = (inputVal * quantity) + Delta();
+                int importantValue2 = (inputVal * yearToDate) + 100;
+                if ((yearToDate - importantValue1) > 100)
+                {
+                    importantValue2 -= 20;
+                }
+                int importantValue3 = importantValue2 * 7;
+                return importantValue3 - 2 * importantValue1;
+            }
+
+            private int Delta()
+            {
+                return 0;
+            }
+        }
+    }
+}
