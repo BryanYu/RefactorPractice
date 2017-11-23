ce4a43313096da99ebd25c62f4ede35f30ec5600
BryanYu <ak78912@hotmail.com>
2017-11-23 22:19:09 +0800

    MoveMethod - After

--- RefactorPractice/MoveMethod/MoveMethod.cs
+++ RefactorPractice/MoveMethod/MoveMethod.cs
@@ -15,40 +15,45 @@ namespace RefactorPractice.MoveMethod
 
             private int _daysOverdrawn;
 
-            public double OverDraftCharge()
-            {
-                if (_type.IsPremium())
-                {
-                    double result = 10;
-                    if (_daysOverdrawn > 7)
-                    {
-                        result += (_daysOverdrawn - 7) * 0.85;
-                    }
-                    return result;
-                }
-                else
-                {
-                    return _daysOverdrawn * 1.75;
-                }
-            }
-
             public double BankCharge()
             {
                 double result = 4.5;
                 if (_daysOverdrawn > 0)
                 {
-                    result += this.OverDraftCharge();
+                    result += this._type.OverDraftCharge(this);
                 }
                 return result;
             }
-        }
-    }
 
-    internal class AccountType
-    {
-        public bool IsPremium()
+            public int GetDaysOverdrawn()
+            {
+                return this._daysOverdrawn;
+            }
+        }
+
+        public class AccountType
         {
-            return true;
+            public bool IsPremium()
+            {
+                return true;
+            }
+
+            public double OverDraftCharge(Account account)
+            {
+                if (IsPremium())
+                {
+                    double result = 10;
+                    if (account.GetDaysOverdrawn() > 7)
+                    {
+                        result += (account.GetDaysOverdrawn() - 7) * 0.85;
+                    }
+                    return result;
+                }
+                else
+                {
+                    return account.GetDaysOverdrawn() * 1.75;
+                }
+            }
         }
     }
 }
