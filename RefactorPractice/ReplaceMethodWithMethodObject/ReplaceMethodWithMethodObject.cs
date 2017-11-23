8f861a4ce119fa3275bdfb30fd7e6581383708c1
BryanYu <ak78912@hotmail.com>
2017-11-23 21:40:07 +0800

    ReplaceMethodWithMethodObject - After

--- RefactorPractice/ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject.cs
+++ RefactorPractice/ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject.cs
@@ -12,19 +12,54 @@ namespace RefactorPractice.ReplaceMethodWithMethodObject
         {
             public int Gamma(int inputVal, int quantity, int yearToDate)
             {
-                int importantValue1 = (inputVal * quantity) + Delta();
+                return new Gamma(this, inputVal, quantity, yearToDate).Compute();
+            }
+
+            public int Delta()
+            {
+                return 0;
+            }
+        }
+
+        public class Gamma
+        {
+            private Account _account;
+
+            private int inputVal;
+
+            private int quantity;
+
+            private int yearToDate;
+
+            private int importantValue1;
+
+            private int importantValue2;
+
+            private int importantValue3;
+
+            public Gamma(Account source, int inputValArg, int quantityArg, int yearToDateArg)
+            {
+                this._account = source;
+                this.inputVal = inputValArg;
+                this.quantity = quantityArg;
+                this.yearToDate = yearToDateArg;
+            }
+
+            public int Compute()
+            {
+                int importantValue1 = (inputVal * quantity) + this._account.Delta();
                 int importantValue2 = (inputVal * yearToDate) + 100;
-                if ((yearToDate - importantValue1) > 100)
-                {
-                    importantValue2 -= 20;
-                }
+                this.ImportantThing();
                 int importantValue3 = importantValue2 * 7;
                 return importantValue3 - 2 * importantValue1;
             }
 
-            private int Delta()
+            private void ImportantThing()
             {
-                return 0;
+                if ((yearToDate - importantValue1) > 100)
+                {
+                    importantValue2 -= 20;
+                }
             }
         }
     }
