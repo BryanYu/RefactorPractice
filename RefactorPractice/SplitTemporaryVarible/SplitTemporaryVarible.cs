4c9308c0346555895ba2df3872838b30b961f007
BryanYu <ak78912@hotmail.com>
2017-11-23 21:05:52 +0800

    SplitTemporaryVarible - After

--- RefactorPractice/SplitTemporaryVarible/SplitTemporaryVarible.cs
+++ RefactorPractice/SplitTemporaryVarible/SplitTemporaryVarible.cs
@@ -19,15 +19,15 @@ namespace RefactorPractice.SplitTemporaryVarible
         public double GetDistanceTravelled(int time)
         {
             double result;
-            double acc = _primaryForce / _mass;
+            double primaryAcc = _primaryForce / _mass;
             int primaryTime = Math.Min(time, _delay);
-            result = 0.5 * acc * primaryTime * primaryTime;
+            result = 0.5 * primaryAcc * primaryTime * primaryTime;
             int secondaryTime = time - this._delay;
             if (secondaryTime > 0)
             {
-                double primaryVel = acc * this._delay;
-                acc = (this._primaryForce + _secondaryForce) / this._mass;
-                result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+                double primaryVel = primaryAcc * this._delay;
+                double secondaryAcc = (this._primaryForce + _secondaryForce) / this._mass;
+                result += primaryVel * secondaryTime + 0.5 * secondaryAcc * secondaryTime * secondaryTime;
             }
             return result;
         }
