bba1c2c6fc5b44245736b551d5a1fd000d179b6e
BryanYu <ak78912@hotmail.com>
2017-11-23 21:58:48 +0800

    SubstituteAlgorithm - Before

---
+++ RefactorPractice/SubstituteAlgorithm/SubstituteAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.SubstituteAlgorithm
+{
+    public class SubstituteAlgorithm
+    {
+        public string FoundPerson(string[] people)
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Equals("Don"))
+                {
+                    return "Don";
+                }
+                if (people[i].Equals("John"))
+                {
+                    return "John";
+                }
+                if (people[i].Equals("Kent"))
+                {
+                    return "Kent";
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
