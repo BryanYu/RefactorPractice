1d8f436d1edd648c7f546568657e574edad54226
BryanYu <ak78912@hotmail.com>
2017-11-23 21:20:34 +0800

    RemovAssignentToParameters - After

--- RefactorPractice/RemovAssignentToParameters/RemovAssignentToParameters.cs
+++ RefactorPractice/RemovAssignentToParameters/RemovAssignentToParameters.cs
@@ -10,22 +10,22 @@ namespace RefactorPractice.RemovAssignentToParameters
     {
         public int Discount(int inputVal, int quantity, int yearToDate)
         {
+            int result = inputVal;
             if (inputVal > 50)
             {
-                inputVal -= 2;
+                result -= 2;
             }
 
             if (quantity > 100)
             {
-                inputVal -= 1;
+                result -= 1;
             }
 
             if (yearToDate > 10000)
             {
-                inputVal -= 4;
+                result -= 4;
             }
-
-            return inputVal;
+            return result;
         }
     }
 }
