411cd2bc289131b1411157c1f572eab011f75f7c
BryanYu <ak78912@hotmail.com>
2017-11-23 21:18:04 +0800

    RemovAssignentToParameters - Before

---
+++ RefactorPractice/RemovAssignentToParameters/RemovAssignentToParameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.RemovAssignentToParameters
+{
+    public class RemovAssignentToParameters
+    {
+        public int Discount(int inputVal, int quantity, int yearToDate)
+        {
+            if (inputVal > 50)
+            {
+                inputVal -= 2;
+            }
+
+            if (quantity > 100)
+            {
+                inputVal -= 1;
+            }
+
+            if (yearToDate > 10000)
+            {
+                inputVal -= 4;
+            }
+
+            return inputVal;
+        }
+    }
+}
