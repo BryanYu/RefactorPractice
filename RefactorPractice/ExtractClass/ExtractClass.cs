bbc38d4db0926d96507eb7c42cbfef1ca4794b9d
BryanYu <ak78912@hotmail.com>
2017-11-25 22:21:45 +0800

    ExtractClass - Before

---
+++ RefactorPractice/ExtractClass/ExtractClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.ExtractClass
+{
+    public class ExtractClass
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public string OfficeAreaCode { get; set; }
+
+            public string OfficeNumber { get; set; }
+
+            public string GetOfficeTelephoneNumber()
+            {
+                return $"({this.OfficeAreaCode}) {this.OfficeNumber}";
+            }
+        }
+    }
+}
