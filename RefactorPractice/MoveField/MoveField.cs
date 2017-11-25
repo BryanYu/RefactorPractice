804a7708d22b03ff5fc1015ef1ead3b5ab4b4297
BryanYu <ak78912@hotmail.com>
2017-11-25 21:59:21 +0800

    MoveField - Before

---
+++ RefactorPractice/MoveField/MoveField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MoveField
+{
+    public class MoveField
+    {
+        public class Account
+        {
+            public AccountType _type;
+
+            private double _interestRate;
+
+            public double InterestForAmonuntDays(double amount, int days)
+            {
+                return this._interestRate * amount * days / 365;
+            }
+        }
+
+        public class AccountType
+        {
+            public double InterestRate { get; set; }
+        }
+    }
+}
