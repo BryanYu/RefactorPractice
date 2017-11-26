976ad34298827dc4a8ac14b3ae1361e6aba7cfaa
BryanYu <ak78912@hotmail.com>
2017-11-26 11:53:35 +0800

    RemoveMiddleMan - Before

---
+++ RefactorPractice/RemoveMiddleMan/RemoveMiddleMan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.RemoveMiddleMan
+{
+    public class RemoveMiddleMan
+    {
+        public void Sample()
+        {
+            var person = new Person();
+            var manager = person.GetManager();
+        }
+
+        public class Person
+        {
+            public Department Department;
+
+            public Person GetManager()
+            {
+                return this.Department.Manager;
+            }
+        }
+
+        public class Department
+        {
+            public string ChargeCode { get; set; }
+
+            public Person Manager { get; }
+
+            public Department(Person manager)
+            {
+                this.Manager = manager;
+            }
+        }
+    }
+}
