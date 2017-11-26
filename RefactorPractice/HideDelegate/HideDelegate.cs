fd7253ed93f8bd3e2b7e3919dd3088fd471fa178
BryanYu <ak78912@hotmail.com>
2017-11-26 11:44:13 +0800

    HideDelegate - Before

---
+++ RefactorPractice/HideDelegate/HideDelegate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.HideDelegate
+{
+    public class HideDelegate
+    {
+        public void Sample()
+        {
+            var person = new Person();
+            var manager = person.Department.Manager;
+        }
+
+        public class Person
+        {
+            public Department Department;
+        }
+
+        public class Department
+        {
+            public string ChargeCode { get; set; }
+
+            public Person Manager { get; }
+
+            public Department(Person manager)
+            {
+                this.Manager = manager;
+            }
+        }
+    }
+}
