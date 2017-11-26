19b54ad2bb392e7222f7eada56a80e8e626f409e
BryanYu <ak78912@hotmail.com>
2017-11-26 11:30:17 +0800

    InlineClass - Before

---
+++ RefactorPractice/InlineClass/InlineClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.InlineClass
+{
+    public class InlineClass
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public TelephoneNumber Type
+            {
+                get
+                {
+                    return new TelephoneNumber();
+                }
+            }
+
+            public string GetTelephoneNumber()
+            {
+                return this.Type.GetTelephoneNumber();
+            }
+        }
+
+        public class TelephoneNumber
+        {
+            public string Number { get; set; }
+
+            public string AreaCode { get; set; }
+
+            public string GetTelephoneNumber()
+            {
+                return $"({this.AreaCode}) {this.Number}";
+            }
+        }
+    }
+}
