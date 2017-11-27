cf1109b5ba8ac10be9dba44b1f1d843000c190c8
BryanYu <ak78912@hotmail.com>
2017-11-27 21:30:53 +0800

    ReplaceDataValueWithObject - Before

---
+++ RefactorPractice/OrganizingData/ReplaceDataValueWithObject/ReplaceDataValueWithObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ReplaceDataValueWithObject
+{
+    public class ReplaceDataValueWithObject
+    {
+        public class Order
+        {
+            private string _customer { get; set; }
+
+            public Order(string customer)
+            {
+                this._customer = customer;
+            }
+
+            private static int NumberOfOrderFor(List<Order> orders, string customer)
+            {
+                var result = 0;
+                foreach (var order in orders)
+                {
+                    if (order._customer.Equals(customer))
+                    {
+                        result++;
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
