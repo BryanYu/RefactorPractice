3797af3db3c5597987429f77d9c3e0ab910540e2
BryanYu <ak78912@hotmail.com>
2017-11-28 21:02:40 +0800

    ChangeValueToReference - Before

---
+++ RefactorPractice/OrganizingData/ChangeValueToReference/ChangeValueToReference.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ChangeValueToReference
+{
+    public class ChangeValueToReference
+    {
+        public class Customer
+        {
+            public string Name { get; set; }
+
+            public Customer(string name)
+            {
+                this.Name = name;
+            }
+        }
+
+        public class Order
+        {
+            public Customer Customer { get; set; }
+
+            public Order(string customerName)
+            {
+                this.Customer = new Customer(customerName);
+            }
+
+            public static int NumberOfOrdersFor(List<Order> orders, string customer)
+            {
+                var result = 0;
+                foreach (var order in orders)
+                {
+                    if (order.GetCusomterName().Equals(customer))
+                    {
+                        result++;
+                    }
+                }
+                return result;
+            }
+
+            public string GetCusomterName()
+            {
+                return this.Customer.Name;
+            }
+        }
+    }
+}
