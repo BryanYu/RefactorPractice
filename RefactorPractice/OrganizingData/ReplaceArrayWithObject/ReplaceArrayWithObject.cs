06d31ce0665380d3c51e3c16cd952ce37910696d
BryanYu <ak78912@hotmail.com>
2017-11-28 21:37:39 +0800

    ReplaceArrayWithObject - Before

---
+++ RefactorPractice/OrganizingData/ReplaceArrayWithObject/ReplaceArrayWithObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ReplaceArrayWithObject
+{
+    public class ReplaceArrayWithObject
+    {
+        public void Sample()
+        {
+            string[] row = new string[3];
+            row[0] = "Liverpool";
+            row[1] = "15";
+            string name = row[0];
+            int wins = Convert.ToInt32(name);
+        }
+    }
+}
