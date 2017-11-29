1726e09442ced7d03f068470f419fb2a4ee3c079
BryanYu <ak78912@hotmail.com>
2017-11-29 10:46:28 +0800

    ChangeUnidirectionalAssociationToBidirectonal - Before

---
+++ RefactorPractice/OrganizingData/ChangeUnidirectionalAssociationToBidirectonal/ChangeUnidirectionalAssociationToBidirectonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ChangeUnidirectionalAssociationToBidirectonal
+{
+    public class ChangeUnidirectionalAssociationToBidirectonal
+    {
+        public class Order
+        {
+            public Customer Customer { get; set; }
+        }
+
+        public class Customer
+        {
+            public List<Order> Orders { get; set; }
+        }
+    }
+}
