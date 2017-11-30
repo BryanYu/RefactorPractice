16eb311491cf564b2890a4c49111b146ae35c976
BryanYu <ak78912@hotmail.com>
2017-11-30 20:58:06 +0800

    ReplaceMagicNumberWithSymbolicConstant - Before

---
+++ RefactorPractice/OrganizingData/ReplaceMagicNumberWithSymbolicConstant/ReplaceMagicNumberWithSymbolicConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ReplaceMagicNumberWithSymbolicConstant
+{
+    public class ReplaceMagicNumberWithSymbolicConstant
+    {
+        public double PotentialEnergy(double mass, double height)
+        {
+            return mass * 9.81 * height;
+        }
+    }
+}
