8a2415fb09f0fa982db8209ef1ee7448ae1784ea
BryanYu <ak78912@hotmail.com>
2017-11-30 21:41:47 +0800

    ReplaceTypeCodeWithClass - Before

---
+++ RefactorPractice/OrganizingData/ReplaceTypeCodeWithClass/ReplaceTypeCodeWithClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ReplaceTypeCodeWithClass
+{
+    public class ReplaceTypeCodeWithClass
+    {
+        public class Person
+        {
+            public static int O = 1;
+
+            public static int A = 2;
+
+            public static int B = 2;
+
+            public static int AB = 3;
+
+            private int _bloodGroup;
+
+            public int BloodGroup
+            {
+                get
+                {
+                    return this._bloodGroup;
+                }
+
+                set
+                {
+                    this._bloodGroup = value;
+                }
+            }
+
+            public Person(int bloodGroup)
+            {
+                this._bloodGroup = bloodGroup;
+            }
+        }
+    }
+}
