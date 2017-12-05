f7350b4bab7c52175b320c309a4420585d667a3c
Bryan <d58526@gmail.com>
2017-12-05 16:25:24 +0800

    ConsolidateDuplicateConditionalFragments - Before

---
+++ RefactorPractice/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragments/ConsolidateDuplicateConditionalFragments.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.SimplifyingConditionalExpressions.ConsolidateDuplicateConditionalFragments
+{
+    internal class ConsolidateDuplicateConditionalFragments
+    {
+        public void Sample()
+        {
+            var total = 0.0D;
+            var price = 0.0D;
+            if (IsSpecialDeal())
+            {
+                total = price * 0.95;
+            }
+            else
+            {
+                total = price * 0.98;
+            }
+            Send();
+        }
+
+        private bool IsSpecialDeal()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
