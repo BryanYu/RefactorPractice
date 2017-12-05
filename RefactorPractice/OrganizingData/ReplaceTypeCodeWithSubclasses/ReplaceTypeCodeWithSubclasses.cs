c79d163926a1f9442cc53b0b467ba00a10c92e0b
Bryan <d58526@gmail.com>
2017-12-05 10:36:45 +0800

    ReplaceTypeCodeWithSubclasses - Before

---
+++ RefactorPractice/OrganizingData/ReplaceTypeCodeWithSubclasses/ReplaceTypeCodeWithSubclasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ReplaceTypeCodeWithSubclasses
+{
+    internal class ReplaceTypeCodeWithSubclasses
+    {
+        public class Employee
+        {
+            private int _type;
+
+            public static int ENGINEER = 0;
+
+            public static int SALESMAN = 1;
+
+            public static int MANAGER = 2;
+
+            public Employee(int type)
+            {
+                this._type = type;
+            }
+        }
+    }
+}
