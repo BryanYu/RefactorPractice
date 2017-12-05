dd21c68c3749b4d98643d840d1b192c5d5be1f2c
Bryan <d58526@gmail.com>
2017-12-05 14:21:11 +0800

    ReplaceSubclassWithFields - Before

---
+++ RefactorPractice/OrganizingData/ReplaceSubclassWithFields/ReplaceSubclassWithFields.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.OrganizingData.ReplaceSubclassWithFields
+{
+    internal class ReplaceSubclassWithFields
+    {
+        public abstract class Person
+        {
+            public abstract bool IsMale();
+
+            public abstract char GetCode();
+        }
+
+        public class Male : Person
+        {
+            public override bool IsMale()
+            {
+                return true;
+            }
+
+            public override char GetCode()
+            {
+                return 'M';
+            }
+        }
+
+        public class Female : Person
+        {
+            public override bool IsMale()
+            {
+                return false;
+            }
+
+            public override char GetCode()
+            {
+                return 'F';
+            }
+        }
+    }
+}
