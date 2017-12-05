a883721573dfadf6f4fddd6c52684ec9f19e18db
Bryan <d58526@gmail.com>
2017-12-05 16:37:35 +0800

    RemoveControlFlag - WithReturn - Before

--- RefactorPractice/SimplifyingConditionalExpressions/RemoveControlFlag/RemoveControlFlag.cs
+++ RefactorPractice/SimplifyingConditionalExpressions/RemoveControlFlag/RemoveControlFlag.cs
@@ -32,5 +32,40 @@ namespace RefactorPractice.SimplifyingConditionalExpressions.RemoveControlFlag
                 throw new NotImplementedException();
             }
         }
+
+        public class WithReturn
+        {
+            private void CheckSecurity(string[] people)
+            {
+                var found = "";
+                for (int i = 0; i < people.Length; i++)
+                {
+                    if (found.Equals(""))
+                    {
+                        if (people[i].Equals("Don"))
+                        {
+                            SendAlert();
+                            found = "Don";
+                        }
+                        if (people[i].Equals("John"))
+                        {
+                            SendAlert();
+                            found = "John";
+                        }
+                    }
+                }
+                SomeLaterCode(found);
+            }
+
+            private void SomeLaterCode(string found)
+            {
+                throw new NotImplementedException();
+            }
+
+            private void SendAlert()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
