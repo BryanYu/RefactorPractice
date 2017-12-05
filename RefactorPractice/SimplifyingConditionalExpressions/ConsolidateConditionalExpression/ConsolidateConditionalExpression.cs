5e410da43918c1e24a148ecab9d810f126f9be59
Bryan <d58526@gmail.com>
2017-12-05 15:12:56 +0800

    ConsolidateConditionalExpression - LogicOR - Before

---
+++ RefactorPractice/SimplifyingConditionalExpressions/ConsolidateConditionalExpression/ConsolidateConditionalExpression.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.SimplifyingConditionalExpressions.ConsolidateConditionalExpression
+{
+    internal class ConsolidateConditionalExpression
+    {
+        public class LogicOR
+        {
+            private int _seniority;
+
+            private int _monthsDisabled;
+
+            private bool _isPartTime;
+
+            private double DisabilityAmount()
+            {
+                if (_seniority < 2) return 0;
+                if (_monthsDisabled > 12) return 0;
+                if (_isPartTime) return 0;
+
+                return 1;
+            }
+        }
+    }
+}
