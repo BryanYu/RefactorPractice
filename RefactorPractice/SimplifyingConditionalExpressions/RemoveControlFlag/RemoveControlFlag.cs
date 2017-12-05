8e8102f11d4068cb47ffc30e7d61a04d1a7e2eff
Bryan <d58526@gmail.com>
2017-12-05 16:35:04 +0800

    RemoveControlFlag - WithBreak  - After

--- RefactorPractice/SimplifyingConditionalExpressions/RemoveControlFlag/RemoveControlFlag.cs
+++ RefactorPractice/SimplifyingConditionalExpressions/RemoveControlFlag/RemoveControlFlag.cs
@@ -12,21 +12,17 @@ namespace RefactorPractice.SimplifyingConditionalExpressions.RemoveControlFlag
         {
             private void CheckSecurity(string[] people)
             {
-                bool found = false;
                 for (int i = 0; i < people.Length; i++)
                 {
-                    if (!found)
+                    if (people[i].Equals("Don"))
                     {
-                        if (people[i].Equals("Don"))
-                        {
-                            SendAlert();
-                            found = true;
-                        }
-                        if (people[i].Equals("John"))
-                        {
-                            SendAlert();
-                            found = true;
-                        }
+                        SendAlert();
+                        break;
+                    }
+                    if (people[i].Equals("John"))
+                    {
+                        SendAlert();
+                        break;
                     }
                 }
             }
