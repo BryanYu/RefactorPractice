037cbacf66b0ee1449c2f2933d4b8850a684ec8a
Bryan <d58526@gmail.com>
2017-12-06 11:30:14 +0800

    ReplaceConditionalWithPolymorphism - Before

---
+++ RefactorPractice/SimplifyingConditionalExpressions/ReplaceConditionalWithPolymorphism/ReplaceConditionalWithPolymorphism.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.SimplifyingConditionalExpressions.ReplaceConditionalWithPolymorphism
+{
+    internal class ReplaceConditionalWithPolymorphism
+    {
+        public class Employee
+        {
+            public const int ENGINERR = 0;
+            public const int SALESMAN = 1;
+            public const int MANAGER = 2;
+
+            private EmployeeType _type;
+
+            private int _monthlySalary;
+
+            private int _commission;
+
+            private int _bonus;
+
+            public int Type
+            {
+                get
+                {
+                    return this._type.GetTypeCode();
+                }
+            }
+
+            private int PayAmount()
+            {
+                switch (this.Type)
+                {
+                    case ENGINERR: return _monthlySalary;
+                    case SALESMAN: return _monthlySalary + _commission;
+                    case MANAGER: return _monthlySalary + _bonus;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+        }
+
+        public abstract class EmployeeType
+        {
+            public abstract int GetTypeCode();
+        }
+
+        public class Engineer : EmployeeType
+        {
+            public override int GetTypeCode()
+            {
+                return Employee.ENGINERR;
+            }
+        }
+
+        public class SalesMan : EmployeeType
+        {
+            public override int GetTypeCode()
+            {
+                return Employee.SALESMAN;
+            }
+        }
+
+        public class Manager : EmployeeType
+        {
+            public override int GetTypeCode()
+            {
+                return Employee.MANAGER;
+            }
+        }
+    }
+}
