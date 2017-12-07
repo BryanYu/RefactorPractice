5dfdf75766f5b5b85f144038c8d0468aa8b52882
Bryan <d58526@gmail.com>
2017-12-07 14:31:49 +0800

    PreserveWholeObject - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/PreserveWholeObject/PreserveWholeObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.PreserveWholeObject
+{
+    internal class PreserveWholeObject
+    {
+        public class Room
+        {
+            public bool WithinPlan(HeatingPlan plan)
+            {
+                int low = DaysTempRange().Low;
+                int high = DaysTempRange().High;
+                return plan.WithinRange(low, high);
+            }
+
+            private TempRange DaysTempRange()
+            {
+                return new TempRange();
+            }
+        }
+
+        public class HeatingPlan
+        {
+            private TempRange _range;
+
+            public bool WithinRange(int low, int high)
+            {
+                return (low > _range.Low && high <= _range.High);
+            }
+        }
+
+        public class TempRange
+        {
+            public int Low { get; set; }
+
+            public int High { get; set; }
+        }
+    }
+}
