f533645086735eb45680a685c6bba3bc4c41ad3e
Bryan <d58526@gmail.com>
2017-12-07 11:01:59 +0800

    SeparateQueryFromModifier - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/SeparateQueryFromModifier/SeparateQueryFromModifier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.SeparateQueryFromModifier
+{
+    internal class SeparateQueryFromModifier
+    {
+        public void CheckSecurity(string[] people)
+        {
+            string found = this.FoundMiscreant(people);
+            SomeLaterCode(found);
+        }
+
+        private void SomeLaterCode(string found)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FoundMiscreant(string[] people)
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Equals("Don"))
+                {
+                    SendAlert();
+                    return "Don";
+                }
+                if (people[i].Equals("John"))
+                {
+                    SendAlert();
+                    return "John";
+                }
+            }
+            return "";
+        }
+
+        private void SendAlert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
