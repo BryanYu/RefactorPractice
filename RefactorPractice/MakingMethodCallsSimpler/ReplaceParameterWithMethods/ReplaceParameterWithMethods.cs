33488929b95ec75b2457ee32ff9385fe4c9e046a
Bryan <d58526@gmail.com>
2017-12-07 15:48:10 +0800

    ReplaceParameterWithMethods - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/ReplaceParameterWithMethods/ReplaceParameterWithMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.ReplaceParameterWithMethods
+{
+    internal class ReplaceParameterWithMethods
+    {
+        private int _itemPrice;
+
+        private int _quantity;
+
+        public double GetPrice()
+        {
+            int basePrice = _quantity * _itemPrice;
+            int discountLevel;
+            if (_quantity > 100)
+            {
+                discountLevel = 2;
+            }
+            else
+            {
+                discountLevel = 1;
+            }
+            double finalPrice = DiscountedPrice(basePrice, discountLevel);
+            return finalPrice;
+        }
+
+        private double DiscountedPrice(int basePrice, int discountLevel)
+        {
+            if (discountLevel == 2)
+            {
+                return basePrice * 0.1;
+            }
+            else
+            {
+                return basePrice * 0.05;
+            }
+        }
+    }
+}
