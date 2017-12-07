2b19382d06951c2d3af0a13bbbb2ee383d8964bc
Bryan <d58526@gmail.com>
2017-12-07 10:36:10 +0800

    RenameMethod - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/RenameMethod/RenameMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.RenameMethod
+{
+    public class RenameMethod
+    {
+        private string _officeNumber;
+
+        private string _officeAreaCode;
+
+        public string GetTelephoneNumber()
+        {
+            return $"({_officeAreaCode}){_officeNumber}";
+        }
+    }
+}
