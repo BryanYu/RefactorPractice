411eb764fea44f91775f191d9fe15ae38d686dfe
Bryan <d58526@gmail.com>
2017-12-07 11:44:15 +0800

    ParameterizeMethod - After

--- RefactorPractice/MakingMethodCallsSimpler/ParameterizeMethod/ParameterizeMethod.cs
+++ RefactorPractice/MakingMethodCallsSimpler/ParameterizeMethod/ParameterizeMethod.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RefactorPractice.MakingMethodCallsSimpler.ParameterizeMethod
@@ -13,16 +14,19 @@ namespace RefactorPractice.MakingMethodCallsSimpler.ParameterizeMethod
         {
             protected double BaseCharge()
             {
-                double result = Math.Min(LastUsage(), 100) * 0.03;
-                if (LastUsage() > 100)
-                {
-                    result += (Math.Min(LastUsage(), 100) - 100) * 0.05;
-                }
-                if (LastUsage() > 200)
-                {
-                    result += (LastUsage() - 200) * 0.07;
-                }
+                double result = UsageInRange(0, 100) * 0.03;
+                result += UsageInRange(100, 200) * 0.05;
+                result += UsageInRange(200, Int32.MaxValue) * 0.07;
                 return result;
+            }
+
+            private double UsageInRange(int start, int end)
+            {
+                if (this.LastUsage() > start)
+                {
+                    return Math.Min(this.LastUsage(), end) - start;
+                }
+                return 0;
             }
 
             private double LastUsage()
