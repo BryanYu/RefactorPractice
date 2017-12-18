ff25ac702d51314c341891a6bab918681405279f
Bryan <d58526@gmail.com>
2017-12-18 15:40:24 +0800

    RemoveSettingMethod - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/RemoveSettingMethod/RemoveSettingMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.RemoveSettingMethod
+{
+    public class RemoveSettingMethod
+    {
+        public class Account
+        {
+            private string _id;
+
+            public Account(string id)
+            {
+                SetId(id);
+            }
+
+            private void SetId(string arg)
+            {
+                this._id = arg;
+            }
+        }
+    }
+}
