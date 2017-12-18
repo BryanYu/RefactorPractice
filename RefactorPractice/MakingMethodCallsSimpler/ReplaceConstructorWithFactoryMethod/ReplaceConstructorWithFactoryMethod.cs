52b565be0920e1d7347b870c51113dffcc37991b
Bryan <d58526@gmail.com>
2017-12-18 16:17:28 +0800

    ReplaceConstructorWithFactoryMethod - ByTypeCode - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/ReplaceConstructorWithFactoryMethod/ReplaceConstructorWithFactoryMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.ReplaceConstructorWithFactoryMethod
+{
+    public class ReplaceConstructorWithFactoryMethodByTypeCode
+    {
+        public void Sample()
+        {
+            var employee = Employee.Create(Employee.ENGINEER);
+        }
+
+        public class Employee
+        {
+            private int _type;
+
+            public const int ENGINEER = 0;
+            public const int SALESMAN = 1;
+            public const int MANAGER = 2;
+
+            public Employee(int type)
+            {
+                this._type = type;
+            }
+
+            public static Employee Create(int type)
+            {
+                return new Employee(type);
+            }
+        }
+    }
+}
