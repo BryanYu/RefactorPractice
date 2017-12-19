96d6939378885120c73b3119229d5baf26788428
Bryan <d58526@gmail.com>
2017-12-19 11:42:46 +0800

    EncapsulateDowncast - Bedore

---
+++ RefactorPractice/MakingMethodCallsSimpler/EncapsulateDowncast/EncapsulateDowncast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.EncapsulateDowncast
+{
+    public class EncapsulateDowncast
+    {
+        public class Reading
+        {
+            private List<Reading> _readings = new List<Reading>();
+
+            public object LastReading()
+            {
+                return this._readings.LastOrDefault();
+            }
+        }
+    }
+}
