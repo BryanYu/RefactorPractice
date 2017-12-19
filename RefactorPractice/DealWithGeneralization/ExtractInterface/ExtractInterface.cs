a293b4bd4fc72252f27f9086459e7d43be30d4da
Bryan <d58526@gmail.com>
2017-12-19 17:42:14 +0800

    ExtractInterface - Before

---
+++ RefactorPractice/DealWithGeneralization/ExtractInterface/ExtractInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.DealWithGeneralization.ExtractInterface
+{
+    public class ExtractInterface
+    {
+        public class Employee
+        {
+            private int _rate;
+
+            public int Rate
+            {
+                get
+                {
+                    return this._rate;
+                }
+            }
+
+            public bool HasSpecialSkill()
+            {
+                return true;
+            }
+        }
+
+        public double Charge(Employee employee, int days)
+        {
+            int result = employee.Rate * days;
+            if (employee.HasSpecialSkill())
+            {
+                return result * 1.05;
+            }
+            return result;
+        }
+    }
+}
