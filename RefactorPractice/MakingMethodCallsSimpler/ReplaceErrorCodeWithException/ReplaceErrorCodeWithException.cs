27768e5a40a72fab410a431f583fadc0c6ce1b4d
Bryan <d58526@gmail.com>
2017-12-19 14:01:03 +0800

    ReplaceErrorCodeWithException - Before

---
+++ RefactorPractice/MakingMethodCallsSimpler/ReplaceErrorCodeWithException/ReplaceErrorCodeWithException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.MakingMethodCallsSimpler.ReplaceErrorCodeWithException
+{
+    public class ReplaceErrorCodeWithException
+    {
+        private int _balance;
+
+        public int Withdraw(int amount)
+        {
+            if (amount > _balance)
+            {
+                return -1;
+            }
+            this._balance -= amount;
+            return 0;
+        }
+    }
+}
