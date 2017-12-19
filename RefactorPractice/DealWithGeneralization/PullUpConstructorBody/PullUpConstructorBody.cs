03536883282f1732fe411e4dc884a722e7039012
Bryan <d58526@gmail.com>
2017-12-19 14:40:12 +0800

    PullUpConstructorBody - Before

---
+++ RefactorPractice/DealWithGeneralization/PullUpConstructorBody/PullUpConstructorBody.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.DealWithGeneralization.PullUpConstructorBody
+{
+    public class PullUpConstructorBody
+    {
+        public class Employee
+        {
+            protected string _name;
+
+            protected string _id;
+        }
+
+        public class Manager : Employee
+        {
+            private int _grade;
+
+            public Manager(string name, string id, int grade)
+            {
+                this._name = name;
+                this._id = id;
+                this._grade = grade;
+            }
+        }
+    }
+}
