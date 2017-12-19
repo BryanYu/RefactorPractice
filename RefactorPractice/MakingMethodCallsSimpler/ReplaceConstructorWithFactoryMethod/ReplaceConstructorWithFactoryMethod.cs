f51cb7d98450e322541fd426d6963bd551794f8a
Bryan <d58526@gmail.com>
2017-12-19 11:01:14 +0800

    ReplaceConstructorWithFactoryMethod - ByString - Before

--- RefactorPractice/MakingMethodCallsSimpler/ReplaceConstructorWithFactoryMethod/ReplaceConstructorWithFactoryMethod.cs
+++ RefactorPractice/MakingMethodCallsSimpler/ReplaceConstructorWithFactoryMethod/ReplaceConstructorWithFactoryMethod.cs
@@ -32,4 +32,45 @@ namespace RefactorPractice.MakingMethodCallsSimpler.ReplaceConstructorWithFactor
             }
         }
     }
+
+    public class ReplaceConstructorWithFactoryMethodByString
+    {
+        public class Employee
+        {
+            private int _type;
+
+            public const int ENGINEER = 0;
+            public const int SALESMAN = 1;
+            public const int MANAGER = 2;
+
+            public static Employee Create(int type)
+            {
+                switch (type)
+                {
+                    case ENGINEER:
+                        return new Engineer();
+
+                    case SALESMAN:
+                        return new SalesMan();
+
+                    case MANAGER:
+                        return new Manager();
+
+                    default: throw new ArgumentException("Incorrect type code value");
+                }
+            }
+        }
+
+        public class SalesMan : Employee
+        {
+        }
+
+        public class Manager : Employee
+        {
+        }
+
+        public class Engineer : Employee
+        {
+        }
+    }
 }
