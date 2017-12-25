98d7d7944053d26c8136516ab3dfd2e65ed61130
BryanYu <ak78912@hotmail.com>
2017-12-25 21:51:09 +0800

    ReplaceDelegationWithInheritance - Before

---
+++ RefactorPractice/DealWithGeneralization/ReplaceDelegationWithInheritance/ReplaceDelegationWithInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.DealWithGeneralization.ReplaceDelegationWithInheritance
+{
+    internal class ReplaceDelegationWithInheritance
+    {
+        public class Employee
+        {
+            private Person _person = new Person();
+
+            public string GetName()
+            {
+                return this._person.Name;
+            }
+
+            public void SetName(string name)
+            {
+                this._person.Name = name;
+            }
+
+            public string ToString()
+            {
+                return "Emp:" + this._person.LastName;
+            }
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public string LastName
+            {
+                get
+                {
+                    return this.Name.Substring(this.Name.LastIndexOf("") + 1);
+                }
+            }
+        }
+    }
+}
