ddf72a32d680d8602ab1bfdd3718135f4926d792
BryanYu <ak78912@hotmail.com>
2017-12-25 21:28:35 +0800

    ReplaceIngeritanceWithDelegation - Before

---
+++ RefactorPractice/DealWithGeneralization/ReplaceIngeritanceWithDelegation/ReplaceIngeritanceWithDelegation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorPractice.DealWithGeneralization.ReplaceIngeritanceWithDelegation
+{
+    internal class ReplaceIngeritanceWithDelegation
+    {
+        public class MyStack : ArrayList
+        {
+            public void Push(Object obj)
+            {
+                base.Insert(0, obj);
+            }
+
+            public Object Pop()
+            {
+                var result = base[0];
+                base.RemoveAt(0);
+                return result;
+            }
+        }
+    }
+}
